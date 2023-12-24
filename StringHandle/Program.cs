864244d15f7b168e6a29e655eb1026eddf814345
Andrey Makarov <mav737@mail.ru>
2023-12-25 00:00:39 +0300

    Code created for input string array

--- StringHandle/Program.cs
+++ StringHandle/Program.cs
@@ -1,3 +1,28 @@
 ﻿Console.Clear(); // Очищаем консоль
-Console.Write("Введите длину массива: ");
-// int x = Convert.ToInt32(Console.ReadLine());
+
+int number;
+while (true) // Бесконечный цикл
+{
+    Console.Write("Введите количество строк для ввода от 1 до 10: ");
+    string input = Console.ReadLine()!; // Чтение строки ввода пользователя
+    
+    if (int.TryParse(input, out number))
+    {
+        if (number>0&&number<11) 
+        {Console.WriteLine($"Введено число {number}");
+        break;
+        }
+        else Console.WriteLine("Веденное число не лежит в диапазоне от 1 до 10!");
+
+    }
+}
+
+string[] InputArray = new string[number];
+
+for (int i = 0; i < number; i++) //Заполняем массив строк
+{
+
+    Console.WriteLine($"Введите строку:{i + 1}");
+    InputArray[i] = Console.ReadLine()!;
+}
+Console.WriteLine($"Задан массив[{string.Join("; ", InputArray)}]");
