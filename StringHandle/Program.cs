a0971b5e03ee6583b18f467f865cf0e403eccff3
Andrey Makarov <mav737@mail.ru>
2023-12-25 08:55:56 +0300

    Console program is modified

--- StringHandle/Program.cs
+++ StringHandle/Program.cs
@@ -1,16 +1,40 @@
 ﻿Console.Clear(); // Очищаем консоль
 
+// Функция формирует новый массив со строками, длина которых не превышает 3х символов
+string[] NewArray(string[] array)
+{
+    // определяем кол-во элементов нового массива
+    int ArrayItem = 0;
+    string[] ArrayNew = new string[ArrayItem];
+
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length <= 3)
+        {
+            ArrayItem++;
+            Array.Resize(ref ArrayNew, ArrayItem);
+            ArrayNew[ArrayItem - 1] = array[i];
+        }
+
+    }
+
+    return ArrayNew;
+
+}
+
 int number;
+string input;
 while (true) // Бесконечный цикл
 {
     Console.Write("Введите количество строк для ввода от 1 до 10: ");
-    string input = Console.ReadLine()!; // Чтение строки ввода пользователя
-    
+    input = Console.ReadLine()!; // Чтение строки ввода пользователя
+
     if (int.TryParse(input, out number))
     {
-        if (number>0&&number<11) 
-        {Console.WriteLine($"Введено число {number}");
-        break;
+        if (number > 0 && number < 11)
+        {
+            Console.WriteLine($"Число строк: {number}");
+            break;
         }
         else Console.WriteLine("Веденное число не лежит в диапазоне от 1 до 10!");
 
@@ -22,7 +46,20 @@ string[] InputArray = new string[number];
 for (int i = 0; i < number; i++) //Заполняем массив строк
 {
 
-    Console.WriteLine($"Введите строку:{i + 1}");
-    InputArray[i] = Console.ReadLine()!;
+    Console.WriteLine($"Введите строку {i + 1}:");
+    while (true)
+    {
+        input = Console.ReadLine()!;
+        if (input != "")
+        {
+            InputArray[i] = input;
+            break;
+        }
+        else Console.WriteLine("Строка пустая, введите строку!");
+    }
 }
-Console.WriteLine($"Задан массив[{string.Join("; ", InputArray)}]");
+Console.WriteLine($"Вы ввели массив из строк: [{string.Join("; ", InputArray)}]");
+// Выводим итоговый массив
+Console.WriteLine($"Итоговый массив: [{string.Join("; ", NewArray(InputArray))}]");
+
+
